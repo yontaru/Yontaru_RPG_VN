dbd424350e2738875e154bb9cc8667707bd3078d
yontaru <70823176+yontaru@users.noreply.github.com>
2024-08-19 16:45:41 -0500

    Transition
    Se agrega una transition del menú a la escena 1

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -3,12 +3,12 @@ extends Control
 var chapter_1 = preload("res://Scenes/chapter_1.tscn")
 
 func _on_play_pressed() -> void:
+	TransitionScene.transition()
+	await TransitionScene.on_transition_finished
 	get_tree().change_scene_to_packed(chapter_1)
-
 
 func _on_options_pressed() -> void:
 	pass # Replace with function body.
-
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
